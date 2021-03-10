e5f08fead61a2d98e67fe4ab26689efae40b864a
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2021-03-09 21:43:33 -0300

    Criando componente de modal

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
@@ -7,9 +6,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
 
+  @Input() modalId;
+
   constructor() { }
 
   ngOnInit(): void {
+    if(this.modalId == null) {
+      throw new Error('Please inform an "modalId" to the <app-modal> component.');
+    }
   }
 
 }
