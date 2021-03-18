a124c9eecc96e827d11e9e6b43d288bf0c374ce7
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2021-03-17 21:52:32 -0300

    Criação do componente de logo

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -7,9 +7,10 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SigninComponent } from './components/signin/signin.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
-  declarations: [LoginComponent, SigninComponent, SignupComponent],
+  declarations: [LoginComponent, SigninComponent, SignupComponent, LogoComponent],
   imports: [
     CommonModule,
     LoginRoutingModule,
